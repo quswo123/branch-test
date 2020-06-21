516b873e19bc21613f1a907d54000c58761ea497
quswo123 <quswo123@naver.com>
2020-06-21 23:11:44 +0900

    ㅇ

--- project/recipe_customer/src/com/recipe/view/CustomerInfoView.java
+++ project/recipe_customer/src/com/recipe/view/CustomerInfoView.java
@@ -8,21 +8,12 @@ public class CustomerInfoView {
 응?
 어?
 어?어!
-<<<<<<< HEAD
 dmd
 dmd
 아
 ㅏㅇ
 ㅏ아
 아
-<<<<<<< HEAD
-=======
-
-
 변재원이 여기다했어???
->>>>>>> jaewon2
-=======
-
 테스터도 여기다했어어
->>>>>>> tester2
 }
